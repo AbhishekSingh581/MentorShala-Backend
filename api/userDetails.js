34c21802df0cd18f9151ac22eda432fea2668ad2
Abhishek <abhiofficial581@gmail.com>
2022-11-05 16:25:15 +0530

    backend update

--- api/userDetails.js
+++ api/userDetails.js
@@ -1,11 +1,17 @@
 import express from "express"
 import MentorShalaDAO from './dao/MentorShalaDAO.js'
+import fetchDetailsCtrl from './controller/fetchDetailsCtrl.js'
 const router=express.Router()
 
 const getMentorShalaUsers=async()=>{
     const list=await MentorShalaDAO.getUser()
     console.log(list);
+    return list
 }
 
-router.route("/").get((req,res)=>{getMentorShalaUsers()})
+router.route("/getUsers").get(async(req,res)=>{
+    const data=await fetchDetailsCtrl.getapiUsers()
+    console.log(data);
+    res.send(data)
+})
 export default router
